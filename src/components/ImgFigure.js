c064096d0b2a41443d77c34f767fde35174b9bf0
skyline10101 <418523223@qq.com>
2017-08-13 00:44:05 +0800

    build imgFigure Structure 1

---
+++ src/components/ImgFigure.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ImgFigure extends React.Component {
+	render (){
+		return (
+			<figure className="img-figure">
+				<img src={this.props.data.imageURL} alt={this.props.data.title}/>
+				<figcaption>
+					<h2 className="img-title">{this.props.data.title}</h2>
+				</figcaption>
+			</figure>
+		);
+	}
+}
+
+export default ImgFigure;
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,15 +2,16 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import ImgFigure from './ImgFigure';
 
 //获取图片相关数据
-let imageDatas = require('../data/imageDatas.json');
+let imageDatas = require('json!../data/imageDatas.json');
 
 //利用自执行函数，将图片名信息转成图片URL路径信息
 imageDatas = (function genImageURL(imageDataArr){
 	for (var i = 0,j = imageDataArr.length; i < j; i++) {
 		var singleImageData = imageDataArr[i];
-		singleImageData.imageURL = require('../images' + singleImageData[i].fileName);
+		singleImageData.imageURL = require('../images/' + singleImageData.fileName);
 
 		singleImageData[i] = singleImageData;
 	}
@@ -21,12 +22,29 @@ imageDatas = (function genImageURL(imageDataArr){
 
 
 
+
+
+
 class AppComponent extends React.Component {
   render() {
+
+  	var controlerUnits = [],
+  		imgFigures = [];
+
+  	imageDatas.forEach(function(value){
+  		imgFigures.push(<ImgFigure data={value}/>);
+  	});
+
+
     return (
      	<section className="stage">
-     		<section className='img-sec'></section>
-     		<nav className='controler-nav'></nav>
+     		
+     		<section className='img-sec'>
+				{imgFigures}
+     		</section>
+     		<nav className='controler-nav'>
+				{controlerUnits}
+     		</nav>
      	</section>
     );
   }
